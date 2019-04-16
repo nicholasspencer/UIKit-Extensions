a77c6ab65eec35bed82fd2b9b6814f710038a766
Nicholas Spencer <nick.switch@gmail.com>
2019-04-16 14:36:58 -0500

    Add xctest func stubs

--- Tests/UIKit+ExtensionsTests/UIViewReusingTests.swift
+++ Tests/UIKit+ExtensionsTests/UIViewReusingTests.swift
@@ -3,26 +3,38 @@
 import XCTest
 @testable import UIKit_Extensions
 
-class UIViewReusingTests: XCTestCase {
+class UITableViewReusingTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var flyweightView: UITableView? {
+        didSet {
+            guard let flyweightView = flyweightView else { return }
         }
+    }
+
+    func test_UITableViewCellReusing() {
+
+    }
+
+    func test_UITableViewHeaderFooterViewReusing() {
+
+    }
+
+}
+
+class UICollectionViewReusingTests: XCTestCase {
+
+    var flyweightView: UICollectionView? {
+        didSet {
+            guard let flyweightView = flyweightView else { return }
+        }
+    }
+
+    func test_UICollectionViewCellReusing() {
+
+    }
+
+    func test_UICollectionReusableViewReusing() {
+
     }
 
 }
