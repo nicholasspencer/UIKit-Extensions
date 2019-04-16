7efc5eb657d77c757d009d184db50359810d04b0
Nicholas Spencer <nick.switch@gmail.com>
2019-04-16 15:29:12 -0500

    Add basic UIViewReusing tests

--- Tests/UIKit+ExtensionsTests/UIViewReusingTests.swift
+++ Tests/UIKit+ExtensionsTests/UIViewReusingTests.swift
@@ -64,6 +64,65 @@ class UITableViewClassReusingTests: XCTestCase {
 
 }
 
+class UITableViewNibReusingTests: XCTestCase {
+
+    var flyweightView: UITableView? {
+        didSet {
+            guard let flyweightView = flyweightView else { return }
+
+            UIViewReusingTableViewCell.registeringNib(with: flyweightView)
+            UIViewReusingTableViewCell.registeringNib(with: flyweightView, usingCellReuseIdentifier: reuseIdentifier)
+            UIViewReusingTableViewHeaderFooterView.registeringNib(with: flyweightView)
+            UIViewReusingTableViewHeaderFooterView.registeringNib(with: flyweightView, usingHeaderFooterViewReuseIdentifier: reuseIdentifier)
+        }
+    }
+
+    override func setUp() {
+        flyweightView = UITableView()
+    }
+
+    override func tearDown() {
+        flyweightView = nil
+    }
+
+    func test_dequeue_cell_automatic() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingTableViewCell.dequeueingCell(for: flyweightView))
+    }
+
+    func test_dequeue_cell_manual() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingTableViewCell.dequeueingCell(for: flyweightView, usingCellReuseIdentifier: reuseIdentifier))
+    }
+
+    func test_dequeue_view_automatic() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingTableViewHeaderFooterView.dequeueingView(for: flyweightView))
+    }
+
+    func test_dequeue_view_manual() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingTableViewHeaderFooterView.dequeueingView(for: flyweightView, usingHeaderFooterViewReuseIdentifier: reuseIdentifier))
+    }
+
+}
+
 class UICollectionViewClassReusingTests: XCTestCase {
 
     var flyweightView: UICollectionView? {
@@ -119,6 +178,70 @@ class UICollectionViewClassReusingTests: XCTestCase {
         }
 
         XCTAssertNotNil(UIViewReusingCollectionReusableView.dequeueingView(for: flyweightView, usingReuseIdentifier: reuseIdentifier))
+    }
+
+}
+
+
+class UICollectionViewNibReusingTests: XCTestCase {
+
+    var flyweightView: UICollectionView? {
+        didSet {
+            guard let flyweightView = flyweightView else { return }
+
+            UIViewReusingCollectionViewCell.registeringNib(with: flyweightView)
+            UIViewReusingCollectionViewCell.registeringNib(with: flyweightView, usingCellReuseIdentifier: reuseIdentifier)
+            UIViewReusingCollectionReusableView.registeringNib(with: flyweightView)
+            UIViewReusingCollectionReusableView.registeringNib(with: flyweightView, usingReuseIdentifier: reuseIdentifier)
+        }
+    }
+
+    override func setUp() {
+        flyweightView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
+    }
+
+    override func tearDown() {
+        flyweightView = nil
+    }
+
+    func test_dequeue_cell_automatic() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingCollectionViewCell.dequeueingCell(for: flyweightView))
+    }
+
+    func test_dequeue_cell_manual() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        XCTAssertNotNil(UIViewReusingCollectionViewCell.dequeueingCell(for: flyweightView, usingCellReuseIdentifier: reuseIdentifier))
+    }
+
+    func test_dequeue_view_automatic() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        let view = UIViewReusingCollectionReusableView.dequeueingView(for: flyweightView)
+
+        XCTAssertNotNil(view)
+    }
+
+    func test_dequeue_view_manual() {
+        guard let flyweightView = flyweightView else {
+            XCTFail("Test is incorrectly setup")
+            return
+        }
+
+        let view = UIViewReusingCollectionReusableView.dequeueingView(for: flyweightView, usingReuseIdentifier: reuseIdentifier)
+
+        XCTAssertNotNil(view)
     }
 
 }
